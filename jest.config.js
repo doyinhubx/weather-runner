29f120f9201b3fededf124037b0413800917c772
Doyin <doyinelugbadebo@gmail.com>
2025-04-18 06:09:29 +0100

    Fix Jest config conflict and enforce 80% coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.test.js']
+  testMatch: ['**/__tests__/**/*.test.js'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
 };
