9197faa9fadf328aeb2391d789bd80f576360bc1
Doyin <doyinelugbadebo@gmail.com>
2025-05-26 12:15:31 +0100

    Integrate dev environment slack notifications

--- weather-app-terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "cloud_run_url" {
-  description = "URL of the deployed Cloud Run service"
-  value       = google_cloud_run_service.weather_app2_dev.status[0].url
-}
-
-output "service_account_email" {
-  description = "Created service account email"
-  value       = google_service_account.weather_app2_sa.email
-}
