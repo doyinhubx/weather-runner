cc6126399be5629edf4610806463545dd184d186
Doyin <doyinelugbadebo@gmail.com>
2025-06-05 12:41:13 +0100

    Update Pulumi stack to use deployer SA for CI/CD and runtime SA for Cloud Run

--- pulumi/__main__.py
+++ pulumi/__main__.py
@@ -262,7 +262,7 @@ app_image = docker.Image(
     )
 )
 
-ServiceTemplateContainerPortArgs
+
 # Deploy Cloud Run service
 cloud_run_service = gcp.cloudrunv2.Service(
     "nodejs-cloudrun-service",
@@ -273,7 +273,7 @@ cloud_run_service = gcp.cloudrunv2.Service(
         containers=[
             gcp.cloudrunv2.ServiceTemplateContainerArgs(
                 image=app_image.image_name,
-                ports=[gcp.cloudrunv2.ServiceTemplateContainerPortArgs(container_port=8080)],
+                ports=[gcp.cloudrunv2.ServiceTemplateContainerPortsArgs(container_port=8080)],
                 envs=[
                     gcp.cloudrunv2.ServiceTemplateContainerEnvArgs(
                         name="NODE_ENV", value="production"
