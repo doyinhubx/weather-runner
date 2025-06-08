350b57aff349fe27de3fafa80581659502a52a39
Doyin <doyinelugbadebo@gmail.com>
2025-06-08 08:30:51 +0100

    chore: testing direct artifact registry deployment

--- terraform/dev/backend.tf
+++ terraform/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket = "YOUR_TERRAFORM_STATE_BUCKET"  # Create this in GCP first
+    bucket = "weather-app-v2"  # Create this in GCP first
     prefix = "dev/cloudrun"
   }
 }
