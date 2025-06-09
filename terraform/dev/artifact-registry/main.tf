8a91d744c1b0e98ac2a038b1f30db17bd7908fa1
Doyin <doyinelugbadebo@gmail.com>
2025-06-09 07:10:13 +0100

    chore (terraform): Import existing Artifact Registry (idempotent)

--- terraform/dev/artifact-registry/main.tf
+++ terraform/dev/artifact-registry/main.tf
@@ -1,5 +1,13 @@
 resource "google_artifact_registry_repository" "weather_appv2" {
+  provider      = google
   location      = var.region
   repository_id = "weather-app-repov2"
   format        = "DOCKER"
+  project       = var.project_id
+  mode          = "STANDARD_REPOSITORY"
+
+  lifecycle {
+    prevent_destroy = true
+    ignore_changes  = [labels]
+  }
 }
