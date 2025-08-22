0363e5e11446e9578a6d57454d7aced3dda84996
Doyin <doyinelugbadebo@gmail.com>
2025-08-22 22:01:08 +0100

    Intentionally cause CI and production deployment jobs to fail in order to verify that the rollback mechanism executes as expected.

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   testMatch: ['**/__tests__/**/*.test.js'],
   coverageThreshold: {
     global: {
-      branches: 0,
+      branches: 80,
       functions: 80,
       lines: 80,
       statements: 80
